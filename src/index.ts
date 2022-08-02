694e0c1ec6ce33556246d5b6b265cbb1bfd56e15
Edgar <avila.agramonte.edg@gmail.com>
2022-08-02 16:53:42 -0500

    Utilizar paquete dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config';
 import express from 'express';
 import useRoutes from './routes';
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 useRoutes(app);
