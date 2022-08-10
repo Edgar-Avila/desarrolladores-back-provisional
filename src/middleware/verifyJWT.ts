21df524f5c973dbc6832654cf93299940c446184
Edgar <avila.agramonte.edg@gmail.com>
2022-08-10 17:27:01 -0500

    Quitar console log de jwt middleware

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -2,17 +2,16 @@ import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import { UserPayload } from '../types/jwt';
 
-export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+export const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if(!authHeader) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, decoded) => {
         if(err){
-            console.log(err);
             return res.sendStatus(401);
         }
         const decodedObject = decoded as UserPayload;
-        res.locals.user = decodedObject.username;
+        res.locals.username = decodedObject.username;
         res.locals.userId = decodedObject.id;
         next();
     });
