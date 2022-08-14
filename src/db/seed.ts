eed63b9951b231d9da1a87ae1865101ea71f059d
Edgar <avila.agramonte.edg@gmail.com>
2022-08-14 16:32:28 -0500

    Mejorado seed de usuarios

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,48 +2,6 @@ import db from './index';
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
 
-function getRandomColor(): string {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function invertColor(hex: string, bw: boolean) {
-    if (hex.indexOf('#') === 0) {
-        hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
-    }
-    var r = parseInt(hex.slice(0, 2), 16),
-        g = parseInt(hex.slice(2, 4), 16),
-        b = parseInt(hex.slice(4, 6), 16);
-    if (bw) {
-        // https://stackoverflow.com/a/3943023/112731
-        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-            ? '#000000'
-            : '#FFFFFF';
-    }
-    // invert color components
-    const sr = (255 - r).toString(16);
-    const sg = (255 - g).toString(16);
-    const sb = (255 - b).toString(16);
-    // pad each with zeros and return
-    return "#" + padZero(sr) + padZero(sg) + padZero(sb);
-}
-
-function padZero(str: string, len: number = 2): string {
-    var zeros = new Array(len).join('0');
-    return (zeros + str).slice(-len);
-}
-
 const main = async () => {
     // Constants
     const languages = ['C++', 'C', 'Python', 'Rust', 'Go', 'Javascript', 'Typescript', 'PHP', 'Lua', 'Java', 'Kotlin'];
@@ -77,19 +35,30 @@ const main = async () => {
     for(let i=0;i<100;i++){
         const hashedPassword = await bcrypt.hash(faker.internet.password(), 10);
         const username = faker.internet.userName();
-        let bg = getRandomColor();
-        let fg = invertColor(bg, true);
-        bg = bg.slice(1);
-        fg = fg.slice(1);
         const user = await db.user.create({
             data: {
                 Username: username,
                 Password: hashedPassword,
-                ProfilePictureUrl: `https://ui-avatars.com/api/?background=${bg}&color=${fg}&name=${username}`
+                ProfilePictureUrl: `https://ui-avatars.com/api/?background=random&name=${username}`
             }
         });
         addedUsers.push(user.UserID);
     }
+    
+    // Default test user
+    {
+        const futureHashedPassword = await bcrypt.hash("future", 10);
+        const futureUsername = "future";
+        const futureUser = await db.user.create({
+            data: {
+                Username: futureUsername,
+                Password: futureHashedPassword,
+                ProfilePictureUrl: `https://ui-avatars.com/api/?background=random&name=${futureUsername}`
+            }
+        });
+        addedUsers.push(futureUser.UserID);
+    }
+
 
     // Posts
     for(let i=0;i<100;i++){
