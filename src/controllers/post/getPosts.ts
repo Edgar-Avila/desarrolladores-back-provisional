01b654b45865d003285339801f542d9b5fa9eaa5
Edgar <avila.agramonte.edg@gmail.com>
2022-08-21 13:13:47 -0500

    Paginacion posts

--- src/controllers/post/getPosts.ts
+++ src/controllers/post/getPosts.ts
@@ -1,15 +1,29 @@
 import { Request, Response } from "express";
 import db from "../../db";
+import { isPositiveInt } from "../../util";
 
 export const getPosts = async (req: Request, res: Response) => {
     let langName = req.query.lang;
+    let page = req.query.page;
+    let pagination = {};
 
     if(langName){
         langName = langName as string;
     }
+    if(page){
+        page = page as string;
+        if(!isPositiveInt(page)){
+            return res.sendStatus(400);
+        }
+        pagination = {
+            skip: (Number(page)-1)*10,
+            take: 10,
+        }
+    }
 
     // Get and send
     let posts = await db.post.findMany({
+        ...pagination,
         include: {
             ProgrammingLanguage: true,
             Comment: true,
