59eb9c1dc2e642c187c3903bdcebfadbfe042722
Edgar <avila.agramonte.edg@gmail.com>
2022-08-21 13:13:36 -0500

    Arreglar error positiveInt

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,6 @@
 export const isPositiveInt = (str: string): boolean => {
     const num = Number(str);
-    if (Number.isInteger(num) || num > 0) {
+    if (Number.isInteger(num) && num > 0) {
         return true;
     }
     return false;
